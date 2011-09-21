c37e8330d93fc2213afc2a38bd9e33e15e3e92bd
Amanda Cockerham <acockerham@gmail.com>
2011-09-20 21:24:40 -0700

    authlogic working, fitgem installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ActiveRecord::Base
-  acts_as_authentic do |c|
-    #c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
-  end # block optional
+  acts_as_authentic
 end
